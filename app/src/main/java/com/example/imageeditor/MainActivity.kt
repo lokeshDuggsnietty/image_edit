eefb142c654bf946f04366cc6343bebd951cffbe
lokeshDuggsnietty <110220256+lokeshDuggsnietty@users.noreply.github.com>
2022-07-29 18:37:25 +0530

    Added flip horizental code

--- app/src/main/java/com/example/imageeditor/MainActivity.kt
+++ app/src/main/java/com/example/imageeditor/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.imageeditor.databinding.ActivityMainBinding
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.net.Uri
+import android.provider.MediaStore
 import android.view.View
 import androidx.exifinterface.media.ExifInterface
 import java.io.IOException
@@ -16,15 +20,18 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var selectedImageUri: Uri? = null
+    private var pic:Bitmap?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.imageButton.setOnClickListener {
+        binding.BSelectImage.setOnClickListener {
             imageChooser();
         }
         binding.editImage.setOnClickListener {
             binding.cropImage.visibility = View.VISIBLE
+            binding.flipHorizental.visibility = View.VISIBLE
+            binding.flipVertical.visibility = View.VISIBLE
         }
         binding.cropImage.setOnClickListener {
             cropImage()
@@ -33,6 +40,12 @@ class MainActivity : AppCompatActivity() {
             getExifData()
         }
 
+        binding.flipHorizental.setOnClickListener {
+            pic?.let { it1 -> flipImage(it1,2) }
+        }
+        binding.flipVertical.setOnClickListener {
+            pic?.let { it1 -> flipImage(it1,1) }
+        }
     }
 
     private fun cropImage() {
@@ -45,6 +58,7 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("outputY",128)
         intent.putExtra("return-data",true)
      startActivityForResult(intent,123)
+
     }
 
     private fun imageChooser() {
@@ -60,23 +74,38 @@ class MainActivity : AppCompatActivity() {
         if (resultCode == RESULT_OK) {
             selectedImageUri = data?.data
             selectedImageUri.let {
-                binding.PreviewImage.setImageURI(selectedImageUri)
-                binding.PreviewImage.drawable.let {
+                binding.IVPreviewImage.setImageURI(selectedImageUri)
+              val imageStream =
+                  selectedImageUri?.let { it1 -> this.getContentResolver().openInputStream(it1) };
+                pic = BitmapFactory.decodeStream(imageStream);
+                binding.IVPreviewImage.drawable.let {
                     binding.editImage.visibility = View.VISIBLE
                     binding.exif.visibility = View.VISIBLE
                 }
             }
         }
-       if (requestCode == 123) {
+        if (requestCode == 123) {
            val extras: Bundle? = data?.extras
-            val pic: Bitmap? = extras?.getParcelable("data")
-            binding.PreviewImage.setImageBitmap(pic)
+             pic = extras?.getParcelable("data")
+            binding.IVPreviewImage.setImageBitmap(pic)
             binding.editImage.visibility = View.GONE
             binding.exif.visibility = View.GONE
             binding.cropImage.visibility = View.GONE
+            binding.flipHorizental.visibility = View.VISIBLE
+            binding.flipVertical.visibility = View.VISIBLE
         }
     }
 
+    fun flipImage(src:Bitmap,type:Int){
+        val matrix:Matrix = Matrix()
+        if(type==1){
+            matrix.preScale(-1.0f,1.0f)
+        }else if (type == 2)
+            matrix.preScale(-1.0f,1.0f)
+        val flipPicture:Bitmap =  Bitmap.createBitmap(src,0,0,src.width,src.height,matrix,true)
+        binding.IVPreviewImage.setImageBitmap(flipPicture)
+        pic = flipPicture
+    }
 
     fun getExifData() {
         var gfgUri: Uri? = selectedImageUri // the file uri
